4c9bf4bc18ddd7ffd976da42b0c0fb1a94b6f311
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-01-17 22:51:30 -0500

    Add files via upload

---
+++ v_smart_reader_ver3.4.4/v_process_text.ino
@@ -0,0 +1,26 @@
+unsigned char process_text(char a[]) {
+
+
+  if (strstr(a, "TOTAL CYCLES") != NULL || strstr(a, "Total Cycles") != NULL) {
+    return TOTAL_CYCLES;
+  }
+
+  else if (strstr(a, "BEGIN") != NULL || strstr(a, "Midmark") != NULL) {
+    _complete = false;
+    return BEGIN;
+  }
+
+  else if (strstr(a, "C1") != NULL)
+    return STOPPED;
+
+  else if (strstr(a, "COMPLETE") != NULL)
+    return CYCLE_COMPLETE;
+
+  else if (strstr(a, "STERILIZING") != NULL && strstr(a, "Drying Complete") == NULL) {
+    eeprom_address = 0;
+    data_log_eeprom = true;
+    return STERILIZING;
+  }
+
+  else return NOTHING_USEFUL;
+}
