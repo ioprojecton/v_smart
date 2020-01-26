99376e8fe50007f819ed5470d114d565d0e8b994
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-01-26 16:15:04 -0500

    Add files via upload

---
+++ v_smart_reader_ver3.5.1/v_send_to_lcd.ino
@@ -0,0 +1,27 @@
+void send_to_lcd(const char a[], unsigned char col, unsigned char row, bool option) {
+
+  if (option) lcd.clear();
+
+  if (strlen(a) > 20) {
+    char b[strlen(a) + 1];
+    memset(b, '\0', sizeof(b));
+
+    for (unsigned char i = 0, c = 0, flag = 0; i < strlen(a); i++) {
+
+      if (a[i] == ' ' && a[i + 1] == ' ') continue;
+
+      else if (a[i] == ' ' && a[i + 1] != ' ' && !flag) {
+        flag = 1;
+        continue;
+      }
+      else b[c++] = a[i];
+    }
+
+    for (unsigned char i = 0; i < (unsigned char)strlen(b); lcd.setCursor(col++, row), lcd.write(b[i++]));
+    return;
+  }
+  for (unsigned char i = 0; i < (unsigned char)strlen(a); lcd.setCursor(col++, row), lcd.write(a[i++]));
+}
+
+
+//
