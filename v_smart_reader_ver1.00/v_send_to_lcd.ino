8ae412a2dd18184677df37c5e35cad4cb89c5fab
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-06-20 17:39:20 -0400

    Delete v_send_to_lcd.ino

--- v_smart_reader_ver1.00/v_send_to_lcd.ino
+++
@@ -1,9 +0,0 @@
-void send_to_lcd(const char a[], unsigned char col, unsigned char row, bool option) {
-
-  if (option) lcd.clear(); //automatically will set to 0,0 position
-
-  for (unsigned char i = 0; (i < (unsigned char)strlen(a)) && strlen(a) <= 20;lcd.setCursor(col++, row), lcd.write(a[i++]));
-}
-
-
-//
