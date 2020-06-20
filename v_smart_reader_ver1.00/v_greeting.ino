7ecdabba040c8ea175ebcd9030362e0a55f5d9eb
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-06-20 17:38:39 -0400

    Delete v_greeting.ino

--- v_smart_reader_ver1.00/v_greeting.ino
+++
@@ -1,8 +0,0 @@
-void greeting(void) {
-
-  send_to_lcd("     Smartreader_V  ", 0, 0, true);
-
-  send_to_lcd(" Datalogger V4.9-M  ", 0, 1, false);
-
- delay(2000); // must write smaller delay function!
-}
