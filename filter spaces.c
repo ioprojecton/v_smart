d0988f37d270730affe59aafb61ceedc260c857c
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-06-20 18:01:37 -0400

    Delete filter spaces.c

--- filter spaces.c
+++
@@ -1,9 +0,0 @@
-char c=0;
-    for(char i=0,flag=0;i<strlen(a);i++){
-        if(a[i]==' ' && a[i+1]==' ') continue;
-        else if(a[i]==' ' && a[i+1]!=' ' && !flag) {
-                flag=1;
-                continue;}
-            else b[c++]=a[i];
-    }
-    b[c]='\0';
