867d86a22cd743b8e08f601b8fc9df451fda645b
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-06-21 21:14:56 -0400

    Delete v_flush.ino

--- v_smart_reader_ver3.5.3/v_flush.ino
+++
@@ -1,9 +0,0 @@
-void my_serial_flush(void) {
-  unsigned long _time = 0;
-  const unsigned int max_time = 1000;
-
-  _time = millis();
-
-  while (millis() - _time <= max_time) Serial1.read();
- 
-}
